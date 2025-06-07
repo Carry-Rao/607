62c3989ebe92d878a7b831326990ff67decbb42d
GitHub Carry-Rao <GitHub carry3658@outlook.com>
2025-06-08 03:11:08 +0800

    modified:   statics/js/safe/token.js

--- statics/js/safe/token.js
+++ statics/js/safe/token.js
@@ -5,31 +5,31 @@ function getToken() {
 }
 
 export function checkToken() {
-    if (!getToken()) {
-        return false;
-    }
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://607backend.carryrao.top/token", true);
-    xhr.setRequestHeader("Authorization", "Bearer " + getToken());
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4) {
-            if (xhr.status == 200) {
-                var response = JSON.parse(xhr.responseText);
-                if (response.success == "true") {
-                    return true;
-                } else {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-        } else {
-            return false;
+    return new Promise((resolve, reject) => {
+        if (!getToken()) {
+            resolve(false);
         }
-    };
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "https://607backend.carryrao.top/token", true);
+        xhr.setRequestHeader("Authorization", "Bearer " + getToken());
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    var response = JSON.parse(xhr.responseText);
+                    if (response.success == "true") {
+                        resolve(true);
+                    } else {
+                        resolve(false);
+                    }
+                } else {
+                    resolve(false);
+                }
+            }
+        };
+        xhr.send();
+    });
 }
 
 export function setToken(token) {
     setCookie("token", token, 30);
 }
-
